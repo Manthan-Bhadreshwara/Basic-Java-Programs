a0f4066e7b82799bffd6c9318d79225ce057c566
Manthan Bhadreshwara <81467433+Manthan-Bhadreshwara@users.noreply.github.com>
2022-03-24 12:45:56 +0530

    Create ArrayEveOddSum.java

---
+++ Arrays/ArrayEveOddSum.java
@@ -0,0 +1,30 @@
+//Program to calculate the sum of even and odd numbers in an Array
+public class ArrayEveOddSum
+{
+	public static void main(String arg[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of the array :");
+		int n=sc.nextInt();
+		
+		int arr[]=new int[n];
+		int sum=0,sumev=0,sumod=0;
+		System.out.println("Enter "+n+"Elements :");
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			sum=sum+arr[i];
+			if(arr[i]%2==0)
+			{
+				sumev=sumev+arr[i];
+			}
+		}
+		System.out.println("Sum of Numbers :"+sum);	
+		System.out.println("Sum of even numbers :"+sumev);
+		System.out.println("Sum of odd number :"+(sum-sumev));
+		
+	}
+}
