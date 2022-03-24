ab99c42e5aa3b40306c71ebd9e8dec96a49c696a
Manthan Bhadreshwara <81467433+Manthan-Bhadreshwara@users.noreply.github.com>
2022-03-24 12:33:44 +0530

    Create ArraySumAvg.java

---
+++ Arrays/ArraySumAvg.java
@@ -0,0 +1,23 @@
+//Program to calcutate the sum and average of elements in an array
+import java.util.Scanner;
+public class ArraySumAvg
+{
+	public static void main(String arg[])	
+	{
+		int sum=0,i,n;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of elements");
+		n=sc.nextInt();
+		int arr[]=new int[n];
+		
+		System.out.println("Enter "+n+" elements of the array ");
+		for(i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+			sum+=arr[i];
+		}
+		System.out.println("Sum : "+sum);
+		double avg = (double)sum/n;
+		System.out.println("Average : "+avg);
+	}
+}
