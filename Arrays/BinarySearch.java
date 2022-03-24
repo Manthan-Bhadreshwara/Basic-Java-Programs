c6ec0d685be059906d3b6042af88f40f5a46a7fb
Manthan Bhadreshwara <81467433+Manthan-Bhadreshwara@users.noreply.github.com>
2022-03-24 13:22:52 +0530

    Update BinarySearch.java

--- Arrays/BinarySearch.java
+++ Arrays/BinarySearch.java
@@ -1,39 +1,44 @@
-import java.util.Scanner;
+//Program to implement Binary Search algorithm
+import java.util.*;
 public class BinarySearch
 {
 	public static void main(String arg[])
 	{
-		Scanner sc = new Scanner(System.in);
-		int arr[] = new int[10];
-		int lb=0,ub=arr.length-1,mid,flag=0;
-		System.out.println("Enter 10 elements :");
-		for(int i=0;i<10;i++)
+		Scanner sc=new Scanner(System.in);
+		int n, lb, ub, mid, val, i;
+		System.out.println("Enter how many elements ");
+		n=sc.nextInt();
+
+		int arr[]=new int[n];
+		lb=0;
+		ub=n-1;
+
+		System.out.println("Enter elements ");
+		for(i=0; i<n; i++)
 		{
 			arr[i]=sc.nextInt();
 		}
-		System.out.println("Enter element to be searched :");
-		int n=sc.nextInt();
+		System.out.println("Enter elements to be search ");
+		val=sc.nextInt();
 		while(lb<=ub)
 		{
-			mid=(lb+ub)/2;
-			if(arr[mid]==n)
-			{
-				System.out.println("Element found at position "+mid);
-				flag=1;
-				break;
-			}
-			else if(arr[mid]>n)
-			{
-				ub=mid-1;
-			}
-			else
+			mid=(ub+lb)/2;
+
+			if(arr[mid]<val)
 			{
 				lb=mid+1;
 			}
+			else if(arr[mid]>val)
+			{
+				ub=mid-1;
+			}
+			else if(arr[mid]==val)
+			{
+				System.out.println(val + " found in array at position " + (mid+1));
+				break;
+			}
 		}
-		if(flag==0)
-		{
-			System.out.println("Element not found ");
-		}
+		if(lb>ub)
+			System.out.println(val + " Not found in array");
 	}
-}
+} 
