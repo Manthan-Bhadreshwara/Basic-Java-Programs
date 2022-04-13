f3ff3bdbcc0bd938135bb45051ca3e33cc4d3fa3
Manthan Bhadreshwara <81467433+Manthan-Bhadreshwara@users.noreply.github.com>
2022-04-13 10:44:35 +0530

    Add files via upload

---
+++ String/Toggle.java
@@ -0,0 +1,28 @@
+//Toggle case?  Hello worlD	hELLO WORLd
+import java.util.Scanner;
+class Toggle
+{
+	public static void main(String arg[])
+	{
+		Scanner sc=new Scanner(System.in);
+		String str, ans="";
+		int i;
+		System.out.println("Enter string ");
+		str=sc.nextLine();
+
+		for(i=0; i<str.length(); i++)
+		{
+			char ch=str.charAt(i);
+			if(ch>='a' && ch<='z')
+			{
+				ch-=32;
+			}
+			else if(ch>='A' && ch<='Z')
+			{
+				ch+=32;
+			}
+			ans=ans+ch;
+		}
+		System.out.println(ans);
+	}
+}
